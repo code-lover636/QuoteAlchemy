35c810e130da362dd0bf4b994e1f999fa531771d
Aravind Ashokan <aravindashokan636@gmail.com>
2023-06-25 11:10:39 +0530

    Add Author

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function fetchRandomQuote() {
     fetch('https://api.quotable.io/random')
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             const quoteResult = document.getElementById('quote-result');
+            const authorResult = document.getElementById('author-result');
             quoteResult.textContent = data.content;
+            authorResult.textContent = data.author
         })
         .catch(error => console.log(error));
 }
